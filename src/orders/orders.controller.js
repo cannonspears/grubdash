224d18f6dd9f4b37711c3009e660c5f9f95d4396
Cannon Spears <cannonspears@gmail.com>
2023-05-08 17:29:41 -0500

    Orders controller

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,9 +1,92 @@
 const path = require("path");
-
-// Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
-
-// Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+function orderExists(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  if (foundOrder) {
+    res.locals.order = foundOrder;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Order id does not match route id: ${orderId}`,
+  });
+}
+
+function orderBodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Order must include a ${propertyName}`,
+    });
+  };
+}
+
+function dishesPropertyIsValid(req, res, next) {}
+
+function create(req, res) {
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+  const newOrder = {
+    id: nextId(),
+    deliverTo,
+    mobileNumber,
+    status,
+    dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+function update(req, res) {
+  const order = res.locals.order;
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  order.deliverTo = deliverTo;
+  order.mobileNumber = mobileNumber;
+  order.status = status;
+  order.dishes = dishes;
+
+  res.json({ data: order });
+}
+
+function destroy() {
+  const { orderId } = req.params;
+  const index = orders.findIndex((order) => order.id === Number(orderId));
+  const deletedOrders = orders.splice(index, 1);
+  res.sendStatus(204);
+}
+
+function list(req, res) {
+  res.json({ data: orders });
+}
+
+module.exports = {
+  create: [
+    orderBodyDataHas("deliverTo"),
+    orderBodyDataHas("mobileNumber"),
+    orderBodyDataHas("status"),
+    orderBodyDataHas("dishes"),
+    dishesPropertyIsValid,
+    create,
+  ],
+  read: [orderExists, read],
+  update: [
+    orderExists,
+    orderBodyDataHas("deliverTo"),
+    orderBodyDataHas("mobileNumber"),
+    orderBodyDataHas("status"),
+    orderBodyDataHas("dishes"),
+    dishesPropertyIsValid,
+    update,
+  ],
+  delete: [orderExists, destroy],
+  list,
+};
